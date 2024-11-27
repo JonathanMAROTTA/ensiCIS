ad20f6d11c15a6975745b19b045a82757469bad5
Achraf BENABDELLAH <achraf.benabdellah@grenoble-inp.org>
2024-11-27 12:51:59 +0100

    feat : added s3 bucket to the infra for the backups

--- terraform/modules/hcp-vault/main.tf
+++ terraform/modules/hcp-vault/main.tf
@@ -10,18 +10,3 @@ terraform {
 data "hcp_vault_secrets_app" "juice_shop" {
   app_name = "sample-app"
 }
-
-output "aws_access_key" {
-  value = data.hcp_vault_secrets_app.juice_shop.secrets.aws_access_key
-  sensitive = true
-}
-
-output "aws_secret_key" {
-  value = data.hcp_vault_secrets_app.juice_shop.secrets.aws_secret_key
-  sensitive = true
-}
-
-output "aws_token" {
-  value = data.hcp_vault_secrets_app.juice_shop.secrets.aws_token
-  sensitive = true
-}
